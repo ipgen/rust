606cf9a1fdfdaeba978bc1bfef826f842e97f6f1
rushmorem <rushmore@webenchanter.com>
2021-03-07 11:51:17 +0200

    update deps

--- src/lib.rs
+++ src/lib.rs
@@ -7,14 +7,11 @@
 //! It exposes only two simple functions `ip` and `subnet`.
 //!
 //! [IPGen Spec]: https://github.com/ipgen/spec
-extern crate blake2;
-extern crate ipnetwork;
-
+use blake2::digest::{Update, VariableOutput};
+use blake2::VarBlake2b;
+use ipnetwork::{Ipv4Network, Ipv6Network};
 use std::net::{IpAddr, Ipv4Addr, Ipv6Addr};
 use std::str::FromStr;
-use blake2::VarBlake2b;
-use blake2::digest::{Input, VariableOutput};
-use ipnetwork::{Ipv4Network, Ipv6Network};
 
 /// Generates an IP address
 ///
@@ -22,7 +19,8 @@ use ipnetwork::{Ipv4Network, Ipv6Network};
 /// eg `fd52:f6b0:3162::/64` or `10.0.0.0/8` and computes a unique IP address.
 pub fn ip(name: &str, cidr: &str) -> Result<IpAddr, String> {
     let ip_addr = match IpAddr::from_str(cidr.split("/").collect::<Vec<&str>>()[0])
-        .map_err(|err| err.to_string()) {
+        .map_err(|err| err.to_string())
+    {
         Ok(ip_addr) => ip_addr,
         Err(msg) => return Err(msg),
     };
@@ -32,9 +30,11 @@ pub fn ip(name: &str, cidr: &str) -> Result<IpAddr, String> {
             match Ipv6Network::from_str(cidr).map_err(|err| format!("{:?}", err)) {
                 Ok(net) => {
                     if net.prefix() == 128 {
-                        return Err(format!("{}/{} is already a full IPv6 address",
-                                           net.ip(),
-                                           net.prefix()));
+                        return Err(format!(
+                            "{}/{} is already a full IPv6 address",
+                            net.ip(),
+                            net.prefix()
+                        ));
                     };
                     return ip6(name, net).map(|ip| IpAddr::V6(ip));
                 }
@@ -46,32 +46,33 @@ pub fn ip(name: &str, cidr: &str) -> Result<IpAddr, String> {
             match Ipv4Network::from_str(cidr).map_err(|err| format!("{:?}", err)) {
                 Ok(net) => {
                     if net.prefix() == 32 {
-                        return Err(format!("{}/{} is already a full IPv4 address",
-                                           net.ip(),
-                                           net.prefix()));
+                        return Err(format!(
+                            "{}/{} is already a full IPv4 address",
+                            net.ip(),
+                            net.prefix()
+                        ));
                     };
                     let ip6prefix = 128 - 32 + net.prefix();
                     let ip6net = format!("::{}/{}", net.ip(), ip6prefix);
-                    match Ipv6Network::from_str(ip6net.as_str())
-                        .map_err(|err| format!("{:?}", err)) {
-                        Ok(net) => {
-                            match ip6(name, net) {
-                                Ok(a) => {
-                                    let a = a.to_string();
-                                    let addr = a.split("::").collect::<Vec<&str>>()[1];
-                                    match Ipv4Addr::from_str(addr) {
-                                        Ok(ip) => return Ok(IpAddr::V4(ip)),
-                                        Err(msg) => {
-                                            return Err(format!("generated IPv4 address ({}) has \
+                    match Ipv6Network::from_str(ip6net.as_str()).map_err(|err| format!("{:?}", err))
+                    {
+                        Ok(net) => match ip6(name, net) {
+                            Ok(a) => {
+                                let a = a.to_string();
+                                let addr = a.split("::").collect::<Vec<&str>>()[1];
+                                match Ipv4Addr::from_str(addr) {
+                                    Ok(ip) => return Ok(IpAddr::V4(ip)),
+                                    Err(msg) => {
+                                        return Err(format!(
+                                            "generated IPv4 address ({}) has \
                                                                 {}",
-                                                               addr,
-                                                               msg))
-                                        }
-                                    };
-                                }
-                                Err(msg) => return Err(msg),
+                                            addr, msg
+                                        ))
+                                    }
+                                };
                             }
-                        }
+                            Err(msg) => return Err(msg),
+                        },
                         Err(msg) => return Err(msg),
                     };
                 }
@@ -90,9 +91,7 @@ fn ip6(name: &str, net: Ipv6Network) -> Result<Ipv6Addr, String> {
     // Uncompress the IP address and throw away the semi-colons
     // so we can easily join extract the network part and later
     // join it to the address part that we will compute.
-    let ip_parts: Vec<String> = ip.iter()
-        .map(|b| format!("{:04x}", b))
-        .collect();
+    let ip_parts: Vec<String> = ip.iter().map(|b| format!("{:04x}", b)).collect();
     let ip_hash = ip_parts.join("");
     let ip_hash = ip_hash.as_str();
     let network_hash = &ip_hash[0..network_len];
@@ -108,28 +107,34 @@ fn ip6(name: &str, net: Ipv6Network) -> Result<Ipv6Addr, String> {
     // remainder (which will aways be 1) we add it back to output length
     // and then discard the last character of the resulting hash.
     let blake_len = (address_len / 2) + (address_len % 2);
-    let address_hash = hash(name, blake_len);
+    let address_hash = hash(name.as_bytes(), blake_len);
     let ip_hash = format!("{}{}", network_hash, address_hash);
-    let ip = format!("{}:{}:{}:{}:{}:{}:{}:{}",
-                     &ip_hash[0..4],
-                     &ip_hash[4..8],
-                     &ip_hash[8..12],
-                     &ip_hash[12..16],
-                     &ip_hash[16..20],
-                     &ip_hash[20..24],
-                     &ip_hash[24..28],
-                     &ip_hash[28..32]);
+    let ip = format!(
+        "{}:{}:{}:{}:{}:{}:{}:{}",
+        &ip_hash[0..4],
+        &ip_hash[4..8],
+        &ip_hash[8..12],
+        &ip_hash[12..16],
+        &ip_hash[16..20],
+        &ip_hash[20..24],
+        &ip_hash[24..28],
+        &ip_hash[28..32]
+    );
     Ipv6Addr::from_str(ip.as_str())
         .map_err(|err| format!("generated IPv6 address ({}) has {}", ip, err))
 }
 
 /// Computes a subnet ID for any identifier
 pub fn subnet(name: &str) -> String {
-    hash(name, 2)
+    hash(name.as_bytes(), 2)
 }
 
-fn hash(name: &str, len: usize) -> String {
-    let mut hash = VarBlake2b::new(len).unwrap();
-    hash.input(name);
-    hash.vec_result().iter().map(|v| format!("{:02x}", v)).collect()
+fn hash(name: &[u8], len: usize) -> String {
+    let mut hasher = VarBlake2b::new(len).unwrap();
+    hasher.update(name);
+    let mut hash = String::with_capacity(len);
+    hasher.finalize_variable(|res| {
+        hash = res.iter().map(|v| format!("{:02x}", v)).collect();
+    });
+    hash
 }
